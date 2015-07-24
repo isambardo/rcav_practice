1dcc0a59b58e20fc6c20bbaba4fce9fb114f1a83
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-07-24 15:04:56 -0500

    squared done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,18 @@
 class CalculationsController < ApplicationController
+
   def instructions
+
     render("instructions.html.erb")
+
   end
+
+  def square
+
+    @user_number = params["user_number"]
+    @user_number = @user_number.to_i
+    @user_number_squared = @user_number * @user_number
+    render("square.html.erb")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/instructions", { :controller => "calculations", :action => "instructions" })
+  get("/square/:user_number", { :controller => "calculations", :action => "square" })
 end
